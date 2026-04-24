5376cc73e0da567aa115540e27d7194811fc73c7
IqraL <IqraLatif159@gmail.com>
2026-04-24 14:27:13 +0100

    Landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
-import { DesktopHeader } from "./components/Headers/DesktopHeader";
-import { MobileHeader } from "./components/Headers/MobileHeader";
-import { useDeviceType } from "./hooks/useDeviceType";
+import { LandingPage } from "./components/Pages/LandingPage";
 
 function App() {
-  const device = useDeviceType();
-
-  return (
-    <div
-      style={{
-        width: "100%",
-        minHeight: "100dvh",
-        backgroundImage:
-          "url('https://hickorys.co.uk/app/uploads/2026/04/Homepage-Image-e1776844602421.png')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        opacity: 1,
-      }}
-    >
-      {(device === "desktop" || device === "tablet") && (
-        <DesktopHeader />
-      )}
-      {device === "mobile" && <MobileHeader />}
-    </div>
-  );
+  return <LandingPage />;
 }
 
 export default App;
---
+++ src/components/Pages/LandingPage.tsx
@@ -0,0 +1,25 @@
+import { useDeviceType } from "../../hooks/useDeviceType";
+import { DesktopHeader } from "../Headers/DesktopHeader";
+import { MobileHeader } from "../Headers/MobileHeader";
+
+export const LandingPage = () => {
+  const device = useDeviceType();
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        minHeight: "100dvh",
+        backgroundImage:
+          "url('https://hickorys.co.uk/app/uploads/2026/04/Homepage-Image-e1776844602421.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        opacity: 1,
+      }}
+    >
+      {(device === "desktop" || device === "tablet") && <DesktopHeader />}
+      {device === "mobile" && <MobileHeader />}
+    </div>
+  );
+};
