3a9e2127aac12f98e208302d9812047124ef55f0
IqraL <IqraLatif159@gmail.com>
2026-05-01 23:42:51 +0100

    updated deviceType

--- src/hooks/useDeviceType.tsx
+++ src/hooks/useDeviceType.tsx
@@ -1,19 +1,21 @@
 import  { useEffect, useState } from "react";
 
 type DeviceType = "desktop" | "mobile" | "tablet";
+
 export const useDeviceType = () => {
-  const [device, setDevice] = useState<DeviceType>("desktop");
+  const getDevice = (): DeviceType => {
+    const width = window.innerWidth;
+
+    if (width < 768) return "mobile";
+    if (width < 1024) return "tablet";
+    return "desktop";
+  };
+
+  const [device, setDevice] = useState<DeviceType>(getDevice);
 
   useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
+    const handleResize = () => setDevice(getDevice());
 
-      if (width < 500) setDevice("mobile");
-      else if (width < 1024) setDevice("tablet");
-      else setDevice("desktop");
-    };
-
-    handleResize();
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
