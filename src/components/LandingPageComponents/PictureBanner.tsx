bf538e77ae01dec1b53e598b34a9c894ddd64bb6
IqraL <IqraLatif159@gmail.com>
2026-05-03 17:37:01 +0100

    changed backgroud color

--- src/components/LandingPageComponents/PictureBanner.tsx
+++ src/components/LandingPageComponents/PictureBanner.tsx
@@ -2,6 +2,9 @@ import { ImageHolder } from "./ImageHolder";
 import draftImg from "../../assets/menu-banner-background.png";
 import { useDeviceType } from "../../hooks/useDeviceType";
 
+// rgb(139, 69, 19)
+// rgb(165, 42, 42) 
+// rgb(178, 34, 34) 
 export const PictureBanner = () => {
   const device = useDeviceType();
 
@@ -12,12 +15,16 @@ export const PictureBanner = () => {
         display: device === "mobile" ? "none" : "grid",
         gridTemplateColumns:
           device === "tablet" ? "repeat(2, 1fr)" : "repeat(5, 1fr)",
-        backgroundColor: "rgb(255 176 5 / 21%)",
+        // backgroundColor: "rgb(255 176 5 / 21%)",
+        // backgroundColor: "rgb(139, 69, 19)",
+        backgroundColor: "rgb(165, 42, 42) ",
+        // backgroundColor: "rgb(178, 34, 34) ",
+
         justifyItems: "center",
         width: "100%",
         height: "30dvh",
         gap: "20px",
-    }}
+      }}
     >
       {device !== "tablet" && (
         <>
