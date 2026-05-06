95d2daa3a0b0aeafec8bced0ade8fe1d3c8222d7
IqraL <IqraLatif159@gmail.com>
2026-05-06 19:00:37 +0100

    menu poping more

--- src/components/Menu/MenuDetailItem.tsx
+++ src/components/Menu/MenuDetailItem.tsx
@@ -40,6 +40,9 @@ export const ItemWrapper = () => {
         flexDirection: "column",
         border: "5px solid #c97e5d",
         alignItems: "center",
+        borderRadius: "16px",
+        backgroundColor: "#fff",
+        boxShadow: "0 10px 25px rgba(0,0,0,0.08)",
       }}
     >
       <img
@@ -53,7 +56,11 @@ export const ItemWrapper = () => {
       <div
         style={{
           padding:
-            device === "mobile" ? "20px 20px 20px 20px" : "50px 0px 50px 50px",
+            device === "mobile" ? "20px 20px 20px 20px" : "50px 50px 50px 50px",
+          fontSize: "15px",
+          lineHeight: "1.6",
+          color: "#5a3e3b",
+          textAlign: "center",
         }}
       >
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla
